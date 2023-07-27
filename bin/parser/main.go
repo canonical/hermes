595d8b7f845a8c69539727b20f5ee1115765f6b3
Chengen Du <chengen.du@canonical.com>
2023-07-27 20:52:51 +0800

    Support subscriber in parser

--- bin/parser/main.go
+++ bin/parser/main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"sort"
 
 	"hermes/common"
+	"hermes/log"
 	"hermes/parser"
 	"hermes/storage"
 
@@ -18,6 +20,14 @@ var (
 	logDir      string
 	outputDir   string
 	storEngine  string
+	mode        string
+)
+
+var (
+	modeMap = map[string]func(){
+		"oneshot": OneshotParser,
+		"daemon":  DaemonizedParser,
+	}
 )
 
 func init() {
@@ -28,35 +38,29 @@ func init() {
 
 	flag.StringVar(&logDir, "log_dir", "/var/log/collector", "The path of log directory")
 	flag.StringVar(&outputDir, "output_dir", homeDir+string("/view"), "The path of view directory")
-	flag.StringVar(&storEngine, "storage_engine", "file", "The storage engine")
+	flag.StringVar(&storEngine, "storage_engine", "file", "The storage engine (file)")
+	flag.StringVar(&mode, "mode", "oneshot", "Mode (oneshot|daemon)")
 	flag.Usage = Usage
 }
 
 func Usage() {
-	fmt.Println("Usage: parser [log_dir] [output_dir]")
+	fmt.Println("Usage: parser [log_dir] [output_dir] [storage_engine] [mode]")
 	flag.PrintDefaults()
 }
 
-func main() {
-	flag.Parse()
-
-	err := common.LoadEnv(metadataDir)
-	if err != nil {
-		logrus.Fatal(err)
-	}
-
+func OneshotParser() {
 	storEngineInst, err := storage.GetStorEngine(storEngine, logDir)
 	if err != nil {
-		logrus.Fatal(err)
+		logrus.Fatalf("Failed to get storage engine, err [%s]", err)
 	}
 
 	logMetas, err := storEngineInst.Load()
 	if err != nil {
-		logrus.Fatal(err)
+		logrus.Fatalf("Failed to load meta by storage engine, err [%s]", err)
 	}
 
 	timestamps := []int64{}
-	for timestamp, _ := range logMetas {
+	for timestamp := range logMetas {
 		timestamps = append(timestamps, timestamp)
 	}
 	sort.Slice(timestamps, func(i, j int) bool { return timestamps[i] < timestamps[j] })
@@ -71,4 +75,44 @@ func main() {
 			logrus.Errorf("Failed to parse timestamp [%d], err [%s]", timestamp, err)
 		}
 	}
+}
+
+func DaemonizedParser() {
+	pubsub, err := common.NewPubSub(common.Sub, common.JobCompleteTopic)
+	if err != nil {
+		logrus.Fatalf("Failed to create pubsub, err [%s]", err)
+	}
+
+	for {
+		var logMetaPub log.LogMetaPubFormat
+		bytes := pubsub.Recv()
+		if err := json.Unmarshal(bytes, &logMetaPub); err != nil {
+			logrus.Errorf("Failed to unmarshal data, err [%s]", err)
+			continue
+		}
+		parser, err := parser.NewParser(logDir, outputDir, logMetaPub.Timestamp, logMetaPub.LogMetadata)
+		if err != nil {
+			logrus.Errorf("Failed to generate parser for timestamp [%d], err [%s]", logMetaPub.Timestamp, err)
+			continue
+		}
+
+		if err := parser.Parse(); err != nil {
+			logrus.Errorf("Failed to parse timestamp [%d], err [%s]", logMetaPub.Timestamp, err)
+		}
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	err := common.LoadEnv(metadataDir)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	modeFunc, isExist := modeMap[mode]
+	if !isExist {
+		logrus.Fatalf("Unsupported mode [%s]", mode)
+	}
+	modeFunc()
 }
